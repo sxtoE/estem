5a14fecb640fe40478e60a93ab73ecb4c1e2369a
lharnaldi <lharnaldi@gmail.com>
2018-10-24 16:46:39 -0300

    Work in progress

--- arnaldi/estem/uv.h
+++ arnaldi/estem/uv.h
@@ -1,9 +1,6 @@
 #ifndef _UV_H_
 #define _UV_H_
 
-const int UVOUT   = A8;   //Salida del sensor
-const int REF_3V3 = A9; //3.3V de encendido en el tablero Arduino
-
 int uv_promedioAnalogRead(int pinToRead);
 float uv_mapfloat(float x, float in_min, float in_max, float out_min, float out_max);
 
